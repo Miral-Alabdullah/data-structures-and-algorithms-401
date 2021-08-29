59ef33ea0b19edf0ddd19bc5c14eda9d529c19ea
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-08-29 19:51:12 +0300

    insertion sort function

---
+++ code-challenges/insertion-sort/app/src/main/java/insertion/sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package insertion.sort;
+
+public class InsertionSort {
+
+    public int[] insertionSort(int[] arr){
+        int j;
+        int temp;
+        for(int i=1; i<arr.length; i++){
+            j = i-1;
+            temp = arr[i];
+
+            while(j>=0 && temp<arr[j]){
+                arr[j+1] = arr[j];
+                j = j-1;
+                arr[j+1] = temp;
+            }
+        }
+        return arr;
+    }
+
+    
+}
