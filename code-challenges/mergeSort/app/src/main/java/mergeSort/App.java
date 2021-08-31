c36bce7dc1273ab0a7fff6c05a4ad69c023b9032
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-09-01 01:38:07 +0300

    Done with the implementation and the test units

--- code-challenges/mergeSort/app/src/main/java/mergeSort/App.java
+++ code-challenges/mergeSort/app/src/main/java/mergeSort/App.java
@@ -8,51 +8,15 @@ public class App {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(new App().getGreeting());
+
+        int arr[] = {5,-12,3,8,16,20,19};
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(arr);
+
     }
 
-    public void mergeSort(int[] arr){
-        int n = arr.length;
 
-        if(n>1){
-            int mid = arr.length/2;
-            int left = Arrays.copyOfRange(int[] left, 0, mid);
-            int right = Arrays.copyOfRange(int [] right, mid, arr.length);
-
-            mergeSort(left);
-            mergeSort(right);
-            merge(left, right, arr);
-        }
-    }
-
-    public void merge(int[] left, int[] right, int[] arr){
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < left.length && j < right.length){
-            if(left[i]<=right[j]){
-                arr[k] = left[i];
-                i = i+1;
-            } else{
-                arr[k] = right[j];
-                j = j+1;
-            }
-            k = k+1;
-        }
-        if (i == left.length){
-            while (i < right.length){
-                arr[k] = right[i];
-                k++;
-                i++;
-            }
-        }else {
-            while (j < left.length){
-                arr[k] = left[j];
-                k++;
-                j++;
-            }
-        }
-    }
 }
--- code-challenges/mergeSort/app/src/test/java/mergeSort/AppTest.java
+++ code-challenges/mergeSort/app/src/test/java/mergeSort/AppTest.java
@@ -3,12 +3,35 @@
  */
 package mergeSort;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    //Happy Path
+    @Test void testMergeSort() throws Exception {
+        int arr[] = {5,-12,3,8,16,20,19};
+
+        MergeSort mergeSort = new MergeSort();
+        assertEquals(8, mergeSort.mergeSort(arr)[3]);
+    }
+
+    //Expected Failure || Edge case
+    @Test void testTheTheMethodDivideAndConquer() throws Exception {
+        int arr[] = new int[0];
+
+        MergeSort mergeSort = new MergeSort();
+        Assertions.assertThrows(Exception.class, ()->{
+            mergeSort.mergeSort(arr);
+        });
+
     }
 }
