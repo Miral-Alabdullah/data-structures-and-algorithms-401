957d0787de3f403116606835eda5f38e0665523e
Miral Alabdullah <miral.alabdallah@gmail.com>
2021-09-13 07:57:00 +0300

    Incomplete

---
+++ Graph/app/src/main/java/Graph/Graph.java
@@ -0,0 +1,33 @@
+package Graph;
+
+import java.util.*;
+
+public class Graph {
+    LinkedList<Integer> vertices[];
+
+
+    public void add(int value){
+        vertices = new LinkedList[value];
+        for (int i = 0; i < value; i++) {
+            vertices[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int vertex1, int vertex2){
+        vertices[vertex1].addFirst(vertex2);
+    }
+
+    public void getNodes(int vertex){
+        List<Integer> listOfNodes = new ArrayList<>();
+        Queue<Integer> breadth = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        breadth.remove(vertex);
+        visited.add(vertex);
+
+        while (!breadth.isEmpty()){
+            int front = breadth.remove();
+            listOfNodes.add(front);
+        }
+    }
+}
